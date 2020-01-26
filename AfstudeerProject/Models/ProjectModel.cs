1cd855cc98df41e0e3f85a62d217ec5ed4aac83d
WoudstraITSolutions <martijnhenriwoudstra@gmail.com>
2020-01-26 20:50:31 +0100

    Initial commit

---
+++ AfstudeerProject/Models/ProjectModel.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AfstudeerProject.Models
+{
+    public class ProjectModel
+    {
+        public int Id { get; set; }
+        public string Description { get; set; }
+
+        public double Cost { get; set; }
+        public double Reveneu { get; set; }
+
+        public int Col { get; set; }
+
+        public int Duration { get; set; }
+    }
+
+    public class ProjectDependencyModel { 
+        public int Id { get; set; }
+
+        public ProjectModel Source { get; set; }
+
+        public ProjectModel Destination { get; set; }
+
+        public DependencyModel DependencyModel { get; set; }
+    }
+
+    public class DependencyModel { 
+        public DependencyType DependencyType { get; set; }
+
+        public float Weight { get; set; }
+    }
+
+    public enum DependencyType
+    {
+        Hard, SoftPos, SoftNeg
+    }
+
+    public class SimModel {
+
+        public SimModel (){
+            Entries = new List<SimEntry>();
+        }
+        public int ProjectId { get; set; }
+        public List<SimEntry> Entries { get; set; }
+    }
+
+    public class SimEntry { 
+        public int Year { get; set; }
+        public double Result { get; set; }
+    }
+}
---
+++ AfstudeerProject/wwwroot/openWindow.js
@@ -0,0 +1,30 @@
+﻿window.openWindow = {
+    showPrompt1: function (text) {
+        return prompt(text, '');
+    },
+    showPrompt2: function (text) {
+        return prompt(text, '');
+    },
+    showPrompt3: function (text) {
+        return prompt(text, '');
+    },
+    showPrompt4: function (text) {
+        return prompt(text, '');
+    },
+    showPrompt5: function (text) {
+        return prompt(text, '');
+    },
+    showPrompt6: function (text) {
+        return prompt(text, '');
+    },
+    adjustLine: function (from, to, hard, pos) {
+        var color = hard == true ? 'black' : (pos == true ? 'green' : 'red')
+        $().connections({
+            from: '#' + from, to: '#' + to,
+            css: {
+                'color': color
+
+            },
+        })
+    }
+};
